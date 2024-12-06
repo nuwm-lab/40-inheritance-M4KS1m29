eb848cd0cda07a336c37ce5331850a165e3a89ea
Viktor Zhukovskyy <zeonet@gmail.com>
2024-12-06 09:57:33 +0200

    Revert "Feedback"

--- Program.cs
+++ Program.cs
@@ -1,78 +1,18 @@
 ﻿using System;
 
-class HalfPlane
+namespace LabWork
 {
-    protected double a1, a2, b;
-
-    // Конструктор для встановлення коефіцієнтів
-    public HalfPlane(double a1, double a2, double b)
+    // Даний проект є шаблоном для виконання лабораторних робіт
+    // з курсу "Об'єктно-орієнтоване програмування та патерни проектування"
+    // Необхідно змінювати і дописувати код лише в цьому проекті
+    // Відео-інструкції щодо роботи з github можна переглянути 
+    // за посиланням https://www.youtube.com/@ViktorZhukovskyy/videos 
+    class Program
     {
-        this.a1 = a1;
-        this.a2 = a2;
-        this.b = b;
-    }
-
-    // Віртуальний метод для виведення коефіцієнтів
-    public virtual void DisplayCoefficients()
-    {
-        Console.WriteLine($"Рівняння півплощини: {a1} * x1 + {a2} * x2 <= {b}");
-    }
-
-    // Віртуальний метод для перевірки належності точки
-    public virtual bool IsPointInRegion(params double[] coordinates)
-    {
-        if (coordinates.Length != 2)
-            throw new ArgumentException("Для півплощини потрібні 2 координати.");
-        return (a1 * coordinates[0] + a2 * coordinates[1]) <= b;
-    }
-}
-
-class HalfSpace : HalfPlane
-{
-    private double a3;
-
-    // Конструктор для встановлення коефіцієнтів півпростор
-    public HalfSpace(double a1, double a2, double a3, double b) : base(a1, a2, b)
-    {
-        this.a3 = a3;
-    }
-
-    // Перевизначення методу для виведення коефіцієнтів
-    public override void DisplayCoefficients()
-    {
-        Console.WriteLine($"Рівняння півпростору: {a1} * x1 + {a2} * x2 + {a3} * x3 <= {b}");
-    }
-
-    // Перевизначення методу для перевірки належності точки
-    public override bool IsPointInRegion(params double[] coordinates)
-    {
-        if (coordinates.Length != 3)
-            throw new ArgumentException("Для півпростору потрібні 3 координати.");
-        return (a1 * coordinates[0] + a2 * coordinates[1] + a3 * coordinates[2]) <= b;
-    }
-}
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        // Динамічне створення об'єктів та використання віртуальних методів
-        HalfPlane[] regions = new HalfPlane[2];
-        regions[0] = new HalfPlane(1, 2, 5); // Об'єкт півплощини
-        regions[1] = new HalfSpace(1, 2, 3, 10); // Об'єкт півпростору
-
-        // Демонстрація роботи віртуальних методів
-        foreach (var region in regions)
+        static void Main(string[] args)
         {
-            region.DisplayCoefficients();
-            if (region is HalfSpace)
-            {
-                Console.WriteLine("Точка (1, 1, 1) належить регіону: " + region.IsPointInRegion(1, 1, 1));
-            }
-            else
-            {
-                Console.WriteLine("Точка (1, 1) належить регіону: " + region.IsPointInRegion(1, 1));
-            }
+            
+            Console.WriteLine("Hello World!");
         }
     }
 }
